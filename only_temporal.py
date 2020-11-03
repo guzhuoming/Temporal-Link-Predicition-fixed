138dab93b1471067e97d255da6bb62ba73ffac57
guzhuoming <15818935515@163.com>
2020-11-03 14:46:03 +0800

    fix: 增加LSTM的att

--- only_temporal.py
+++ only_temporal.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
-from numpy import concatenate
 import math
 from math import sqrt
 import csv
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, LSTM, Bidirectional
+from keras.models import Sequential, Model
+from keras.layers import Lambda, dot, Activation, concatenate, Input, Dense, Dropout, LSTM, Bidirectional, Layer
+import keras.backend as K
 from sklearn.preprocessing import MinMaxScaler
 from matplotlib import pyplot as plt
 from sklearn.metrics import mean_squared_error
@@ -15,6 +15,30 @@ set_random_seed(2)
 name = open('./data/name.csv')
 df_name = pd.read_csv(name)
 name_node_pairs = df_name['name_node_pairs']
+
+
+
+class attention(Layer):
+    def __init__(self,**kwargs):
+        super(attention,self).__init__(**kwargs)
+
+    def build(self,input_shape):
+        self.W=self.add_weight(name="att_weight",shape=(input_shape[-1],1),initializer="normal")
+        self.b=self.add_weight(name="att_bias",shape=(input_shape[1],1),initializer="zeros")
+        super(attention, self).build(input_shape)
+
+    def call(self,x):
+        et=K.squeeze(K.tanh(K.dot(x,self.W)+self.b),axis=-1)
+        at=K.softmax(et)
+        at=K.expand_dims(at,axis=-1)
+        output=x*at
+        return K.sum(output,axis=1)
+
+    def compute_output_shape(self,input_shape):
+        return (input_shape[0],input_shape[-1])
+
+    def get_config(self):
+        return super(attention,self).get_config()
 
 def la_ha():
     for i in range(len(name_node_pairs)):
@@ -102,9 +126,21 @@ def lstm(n_features=4,
     n_test = 12 - n_train
 
     # define LSTM
-    model = Sequential()
-    model.add(Bidirectional(LSTM(n_units, input_shape=(n_window, n_features))))
-    model.add(Dense(1))
+    # sequential
+    # model = Sequential()
+    # model.add(Bidirectional(LSTM(n_units, input_shape=(n_window, n_features))))
+    # model.add(Dense(1))
+    #
+    # model.compile(loss='mse', optimizer='adam')
+
+    # Model
+    inputs = Input(shape=(n_window, n_features))
+    att_in = Bidirectional(LSTM(n_units, input_shape=(n_window, n_features), return_sequences=True))(inputs)
+    print('att_in.shape{}'.format(att_in.shape))
+    att_out = attention()(att_in)
+    print('att_out.shape{}'.format(att_out.shape))
+    outputs = Dense(1)(att_out)
+    model = Model(inputs, outputs)
     model.compile(loss='mse', optimizer='adam')
 
     # fit network
@@ -113,7 +149,6 @@ def lstm(n_features=4,
         # plot history
         # plt.plot(history.history['loss'])
         # plt.show()
-
     # make prediction
     inv_yhat = []
     for i in range(n_test):
@@ -127,11 +162,11 @@ def lstm(n_features=4,
     inv_yhat = inv_yhat.T
     print('inv_yhat.shape:{}'.format(inv_yhat.shape)) #inv_yhat.shape:(736, 3)
 
-    inv_yhat = concatenate((scaled_data[:, n_train:, 0], inv_yhat), axis=1)
+    inv_yhat = np.concatenate((scaled_data[:, n_train:, 0], inv_yhat), axis=1)
     inv_yhat = inv_yhat.reshape((n_samples, n_test, 2))
-    inv_yhat = concatenate((inv_yhat, scaled_data[:, n_train:, 2:]), axis=2)
+    inv_yhat = np.concatenate((inv_yhat, scaled_data[:, n_train:, 2:]), axis=2)
     print('inv_yhat.shape:{}'.format(inv_yhat.shape))
-    inv_yhat = concatenate((scaled_data[:, :n_train, :], inv_yhat), axis=1)
+    inv_yhat = np.concatenate((scaled_data[:, :n_train, :], inv_yhat), axis=1)
     print('inv_yhat.shape:{}'.format(inv_yhat.shape))
     inv_yhat = inv_yhat.reshape(n_samples, n_timesteps*n_features)
     print('inv_yhat.shape:{}'.format(inv_yhat.shape))
@@ -142,7 +177,7 @@ def lstm(n_features=4,
     prediction = prediction.reshape(prediction.shape[0], prediction.shape[1], 1)
     original = data[:, -3:, 1]
     original = original.reshape(original.shape[0], original.shape[1], 1)
-    concat = concatenate((original, prediction), axis=2)
+    concat = np.concatenate((original, prediction), axis=2)
     print('concat.shape:{}'.format(concat.shape))
     np.set_printoptions(threshold=1e6)
     print('concat\n{}'.format(concat))
@@ -157,4 +192,4 @@ def lstm(n_features=4,
 if __name__=='__main__':
     # print('only_temporal')
     # la_ha()
-    lstm(4)
+    lstm(4, n_epochs=100)
