061a9821d4acfdc0ba989315e4dd1b2ff6502e42
guzhuoming <15818935515@163.com>
2020-11-05 15:24:05 +0800

    add: 添加xgboost

--- only_temporal.py
+++ only_temporal.py
@@ -11,6 +11,9 @@ from sklearn.preprocessing import MinMaxScaler
 # from matplotlib import pyplot as plt
 from sklearn.metrics import mean_squared_error
 from statsmodels.tsa.arima_model import ARIMA
+from sklearn.metrics import mean_absolute_error
+from sklearn.ensemble import RandomForestRegressor
+# from xgboost import XGBRegressor
 from tensorflow import set_random_seed
 set_random_seed(2)
 
@@ -243,12 +246,15 @@ def arima():
         pred = []
         try:
             for t in range(len(test)):
-                model = ARIMA(history, order=(1,1,0))
+                model = ARIMA(history, order=(0,0,2))
                 model_fit = model.fit(disp=0)
                 output = model_fit.forecast()
                 yhat = output[0]
-                pred.append(yhat)
+                if yhat[0]<0:
+                    yhat[0] = 0
+                pred.append(yhat[0])
                 history.append(test[t])
+            print('pred_>=0:{}'.format(pred))
             mse = mse + mean_squared_error(test, pred)
             print(mse)
         except:
@@ -259,8 +265,114 @@ def arima():
     print('errornum:{}'.format(error))
     print('arima, rmse: {}'.format(rmse))
 
+"""
+random forest
+"""
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+    n_vars = 1 if type(data) is list else data.shape[0]
+    df = pd.DataFrame(data)
+    cols = list()
+    # input sequence (t-n, ... t-1)
+    for i in range(n_in, 0, -1):
+        cols.append(df.shift(i))
+     # forecast sequence (t, t+1, ... t+n)
+    for i in range(0, n_out):
+        cols.append(df.shift(-i))
+    # put it all together
+    agg = pd.concat(cols, axis=1)
+    # drop rows with NaN values
+    if dropnan:
+        agg.dropna(inplace=True)
+    return agg.values
+# split a univariate dataset into train/test sets
+def train_test_split(data, n_test):
+    return data[:-n_test, :], data[-n_test:, :]
+# fit an random forest model and make a one step prediction
+def random_forest_forecast(train, testX):
+    # transform list into array
+    train = np.asarray(train)
+    # split into input and output columns
+    trainX, trainy = train[:, :-1], train[:, -1]
+    # fit model
+    model = RandomForestRegressor(n_estimators=1000)
+    model.fit(trainX, trainy)
+    # make a one-step prediction
+    yhat = model.predict([testX])
+    return yhat[0]
+# walk-forward validation for univariate data
+def walk_forward_validation(data, n_test, methods='randomforest'):
+    predictions = list()
+    # split dataset
+    train, test = train_test_split(data, n_test)
+    # seed history with training dataset
+    history = [x for x in train]
+    # step over each time-step in the test set
+    for i in range(len(test)):
+        # split test row into input and output columns
+        testX, testy = test[i, :-1], test[i, -1]
+        # fit model on history and make a prediction
+        if methods=='randomforest':
+            yhat = random_forest_forecast(history, testX)
+        elif methods=='xgboost':
+            yhat = xgboost_forecast(history, testX)
+        # store forecast in list of predictions
+        predictions.append(yhat)
+        # add actual observation to history for the next loop
+        history.append(test[i])
+        # summarize progress
+        # print('>expected=%.1f, predicted=%.1f' % (testy, yhat))
+    # estimate prediction error
+    error = mean_squared_error(test[:, -1], predictions)
+    return error, test[:, -1], predictions
+def xgboost_forecast(train, testX):
+    # transform list into array
+    train = np.asarray(train)
+    # split into input and output columns
+    trainX, trainy = train[:, :-1], train[:, -1]
+    # fit model
+    model = XGBRegressor(objective='reg:squarederror', n_estimators=1000)
+    model.fit(trainX, trainy)
+    # make a one-step prediction
+    yhat = model.predict(np.asarray([testX]))
+    return yhat[0]
+
+def randomforest():
+    mse = 0
+    for i in range(len(name_node_pairs)):
+        print(i)
+        f = open('./data/features_4/{}_temp_link_ft.csv'.format(name_node_pairs[i]))
+        df = pd.read_csv(f)
+
+        # load the dataset
+        values = df['tran_sum'].values
+        # transform the time series data into supervised learning
+        data = series_to_supervised(values, n_in=6)
+        # evaluate
+        mse_, y, yhat = walk_forward_validation(data, 2, methods='randomforest')
+        mse = mse+mse_
+    rmse = np.sqrt(mse / len(name_node_pairs))
+    print('randomforest, rmse: {}'.format(rmse))
+
+def xgboost():
+    mse = 0
+    for i in range(len(name_node_pairs)):
+        print(i)
+        f = open('./data/features_4/{}_temp_link_ft.csv'.format(name_node_pairs[i]))
+        df = pd.read_csv(f)
+
+        # load the dataset
+        values = df['tran_sum'].values
+        # transform the time series data into supervised learning
+        data = series_to_supervised(values, n_in=6)
+        # evaluate
+        mse_, y, yhat = walk_forward_validation(data, 2, methods='xgboost')
+        mse = mse+mse_
+    rmse = np.sqrt(mse / len(name_node_pairs))
+    print('xgboost, rmse: {}'.format(rmse))
+
 if __name__=='__main__':
     # print('only_temporal')
     # la_ha(n_train=10)
     # lstm(n_features=4, n_epochs=100, with_att=True, n_train=10, n_window=7, methods='lstm', lr=0.001)
-    arima()
+    # arima()
+    randomforest()
